5ae3515cc67b5315b6d11771339fa4f853707963
= <=>
2020-02-21 20:53:51 +0800

    2020.2.21 test how to give a button click function

--- assets/scripts/button1.js
+++ assets/scripts/button1.js
@@ -1,17 +1,15 @@
 // Learn cc.Class:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/class.html
-//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/class.html
+//  - https://docs.cocos.com/creator/manual/en/scripting/class.html
 // Learn Attribute:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
-//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/reference/attributes.html
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
 // Learn life-cycle callbacks:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
-//  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
 
 cc.Class({
     extends: cc.Component,
 
     properties: {
+        button:cc.button
         // foo: {
         //     // ATTRIBUTES:
         //     default: null,        // The default value will be used only when the component attaching
@@ -31,15 +29,14 @@ cc.Class({
 
     // LIFE-CYCLE CALLBACKS:
 
-    onLoad:function () {
-        
-    },
+    // onLoad () {},
 
-    start:function () {
+    start () {
 
     },
-
-    update:function (dt) {
-
+    gotoPage:function(){
+        cc.director.loadScene("log")
     },
+
+    // update (dt) {},
 });
